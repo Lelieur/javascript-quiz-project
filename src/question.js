fcf955b2d8285d64ecd81d0dd74c4f7f885229af
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-10-21 14:23:15 +0200

    Question class created

--- src/question.js
+++ src/question.js
@@ -1,7 +1,33 @@
 class Question {
     // YOUR CODE HERE:
     //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(textValue, choicesValue, answerValue, difficultyValue) {
+        this.text = textValue
+        this.choices = choicesValue
+        this.answer = answerValue
+        this.difficulty = difficultyValue
+    }
+    shuffleChoices() {
+        let randomChoices = [];
+        let n = this.choices.length;
+        let i
 
-    // 2. shuffleChoices()
+        if (n) {
+            i = Math.floor(Math.random() * this.choices.length);
+
+            if (i in this.choices) {
+                randomChoices.push(this.choices[i]);
+                delete this.choices[i];
+                n--
+            }
+
+        }
+
+        return randomChoices;
+
+    }
 }
+
+
+
+
