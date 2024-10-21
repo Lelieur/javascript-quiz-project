72d89d0ed69a6981fbd97fed69019daa359df8f4
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-10-21 15:54:46 +0200

    Question class and Quiz class created

--- src/question.js
+++ src/question.js
@@ -24,7 +24,6 @@ class Question {
         }
 
         return randomChoices;
-
     }
 }
 
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,67 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questionsValue, timeLimitValue, timeRemainingValue) {
+        this.questions = questionsValue
+        this.timeLimit = timeLimitValue
+        this.timeRemaining = timeRemainingValue
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
 
-    // 4. shuffleQuestions()
+        let currentQuestion
 
-    // 5. checkAnswer(answer)
+        currentQuestion = this.questions[this.currentQuestionIndex]
 
-    // 6. hasEnded()
+        return currentQuestion
+    }
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
+
+    shuffleQuestions() {
+        let randomQuestion = [];
+        let n = this.questions.length;
+        let i
+
+        if (n) {
+            i = Math.floor(Math.random() * this.questions.length);
+
+            if (i in this.questions) {
+                randomQuestion.push(this.questions[i]);
+                delete this.questions[i];
+                n--
+            }
+
+        }
+
+        return randomQuestion;
+    }
+
+    checkAnswer(answer) {
+
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+
+            this.correctAnswers++
+
+        }
+
+    }
+
+    hasEnded() {
+
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        } else if (this.currentQuestionIndex = this.questions.length) {
+            return true
+        }
+
+    }
 }
+
+
+
+
